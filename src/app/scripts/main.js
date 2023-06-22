547748929b80072670ccfba571bd34535817bea7
mrcsbda <macbedoya@gmail.com>
2023-06-22 13:46:40 -0500

    print main pokemon

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,9 +1,17 @@
 import "../styles/styles.scss"
+import printMainPokemon from "./UI/printPokemon"
+import getPokemon from "./services/getPokemon"
 import getPokemons from "./services/getPokemons"
 
-const prueba = async () => {
+document.addEventListener('DOMContentLoaded', async ()=> {
     const data = await getPokemons()
-    console.log(data)
-}
+    const pokemon = await getPokemon(data[0])
+    console.log(pokemon.sprites.other.dream_world.front_default)
+    printMainPokemon(pokemon)
+})
 
-prueba()
+// const prueba = async () => {
+//     const data = await getPokemons()
+//     console.log(data)
+// }
+
